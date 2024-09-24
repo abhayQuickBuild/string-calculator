c680bd25ff6b2ee3b66300d1018bc9463a1a3387
abhayQuickBuild <guptaabhay4998@gmail.com>
2024-09-24 18:47:26 +0530

    add logic updated

--- app/calculator/utils.js
+++ app/calculator/utils.js
@@ -1,8 +1,21 @@
 export function add(numbers) {
     if (numbers === "") return 0;
   
-    const delimiters = /,|\n/;
-    const numArray = numbers.split(delimiters).map(Number);
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n", 2); 
+      delimiter = new RegExp(parts[0].substring(2)); 
+      numbers = parts[1]; 
+    }
+
+    const numArray = numbers.split(delimiter);
+    const output = numArray.flatMap(item => item.split('\\n').map(Number));
+
+    const negatives = output.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(', ')}`);
+    }
   
-    return numArray.reduce((acc, num) => acc + num, 0);
+    return output.reduce((acc, num) => acc + num, 0);
   }
+  
