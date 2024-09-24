b4db02e630005b4352b979c15ec7d40d0815ce56
abhayQuickBuild <guptaabhay4998@gmail.com>
2024-09-24 19:07:53 +0530

    jest setup

---
+++ app/calculator/utils.test.js
@@ -0,0 +1,21 @@
+import { add } from './stringCalculator';
+
+test('should return 0 for an empty string', () => {
+  expect(add("")).toBe(0);
+});
+
+test('should return sum of numbers separated by commas', () => {
+  expect(add("1,2,3")).toBe(6);
+});
+
+test('should handle new lines between numbers', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+test('should handle different delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
+
+test('should throw an error for negative numbers', () => {
+  expect(() => add("1,-2")).toThrow('negative numbers not allowed: -2');
+});
